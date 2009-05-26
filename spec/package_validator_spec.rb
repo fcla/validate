3dd201b68eaf8fe0a2b862bb5e17d539c85d5354
manny <manny@e2ae85e3-cf20-0410-855b-be2d16f42433>
2009-05-26 18:12:49 +0000

    re-wrote specs
    git-svn-id: svn://tupelo.fcla.edu/shades/validate-service@1829 e2ae85e3-cf20-0410-855b-be2d16f42433

--- spec/package_validator_spec.rb
+++ spec/package_validator_spec.rb
@@ -1,167 +1,274 @@
 require 'package_validator'
 require 'pp'
-require 'libxml'
 require 'executor'
 require 'configuration'
 require 'digest/md5'
 
 describe PackageValidator do
 
+
+  ALL_OK_PACKAGE = "spec/SamplePackages/FDA0000001"
+  DOES_NOT_EXIST_ON_FILESYSTEM = "foobar"
+  NOT_A_DIRECTORY_ON_FILESYSTEM = "Rakefile"
+  DOES_NOT_CONTAIN_DESCRIPTOR = "lib"
+  DESCRIPTOR_IS_NOT_FILE = "spec/SamplePackages/FDA0000002"
+  NO_CONTENT_FILES = "spec/SamplePackages/FDA0000003"
+  FILE_REFERENCED_BUT_MISSING = "spec/SamplePackages/FDA0000004"
+  CHECKSUM_MISMATCH = "spec/SamplePackages/FDA0000005"
+
   before(:each) do
     @validator = PackageValidator.new
-
-    # stubs for File methods
-    File.stub!(:exists?).and_return true
-    File.stub!(:file?).and_return true
-    File.stub!(:directory?).and_return true
-    File.stub!(:writable?).and_return true
-    File.stub!(:readable?).and_return true
-    File.stub!(:read).and_return ""
-
-    # stubs for Configuration
-    Configuration.stub!(:virus_checker_executable).and_return "/bin/true"
-    Configuration.stub!(:virus_exit_status_clean).and_return 0
-    Configuration.stub!(:virus_exit_status_infected).and_return 1
-
-    # mock filesystem 
-    mock_paths = "/path/to/package/package_name", 
-                 "/path/to/package/package_name/content_file.tif",
-                 "/path/to/package/package_name/package_name.xml"
-
-    # mock account info
-    account_info = {"ACCOUNT"=>"FDA", "PROJECT"=>"FDA"}
-
-    # mock file info
-    file_info = {"CHECKSUM" => "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "href" => "content_file.tif"}
-
-    # mock exit status array
-    stub_return_status = {"exit_status" => 0, "STDOUT" => "", "STDERR" => ""}
-
-    # stub Find.find, to mock directory traversal
-    Find.stub!(:find).and_yield(mock_paths[0]).and_yield(mock_paths[1]).and_yield(mock_paths[2])
-
-    # stub Executor.execute_return_summary
-    Executor.stub!(:execute_return_summary).and_return stub_return_status
-
-    # mock XML objects, documents and nodes
-    @mock_doc = mock 'mock XML document' 
-    @mock_node = mock 'mock XML node'
-    @mock_namespace_node = mock 'mock XML namespace node'
-    @mock_file_node = mock 'mock METS filesec node'
-
-    @mock_doc.stub!(:root).and_return @mock_node
-    @mock_doc.stub!(:find).and_return [@mock_file_node]
-    @mock_doc.stub!(:find_first).and_return @mock_node
-    @mock_doc.stub!(:validate_schema).and_return true
-
-    @mock_node.stub!(:children).and_return true
-    @mock_node.stub!(:namespace_node).and_return @mock_namespace_node
-    @mock_node.stub!(:attributes).and_return account_info
-
-    @mock_file_node.stub!(:attributes).and_return file_info
-    @mock_file_node.stub!(:child).and_return @mock_file_node
-
-    @mock_namespace_node.stub!(:prefix).and_return "METS" 
-
-    # stub LibXML methods
-    LibXML::XML::Schema.stub!(:new).and_return true
-    LibXML::XML::Document.stub!(:file).and_return @mock_doc
-     
-    # stub Digest::MD5 methods
-    Digest::MD5.stub!(:hexdigest).and_return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
   end
   
-  it "validate_pacakge should raise ValidationFailedSyntax if specified package path is not a directory" do
-    File.stub!(:directory?).and_return false
+  # returns a configuration hash that if used, will simulate a virus found with output on STDOUT and STDERR
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedSyntax)
+  def virus_found_hash
+    {"virus_checker_executable" => "echo foo; echo bar 1>&2; /usr/bin/false",
+     "virus_exit_status_infected" => 1,
+     "virus_exit_status_clean" => 0}
   end
 
-  it "validate_pacakge should raise ValidationFailedSyntax if specified package path is not readable" do
-    File.stub!(:readable?).and_return false
+  it "should validate ALL_OK package, and reflect all checks passed" do
+    hash = @validator.validate_package ALL_OK_PACKAGE
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedSyntax)
+    hash["outcome"].should == "success"
+
+    # package syntax checks
+    hash["syntax"]["descriptor_found"].should == "success"
+    hash["syntax"]["descriptor_is_file"].should == "success"
+    hash["syntax"]["content_file_found"].should == "success"
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"]["FDA0000001.xml"]["outcome"].should == "success"
+    hash["virus_check"]["daitss.jpg"]["outcome"].should == "success"
+    hash["virus_check"]["diamondlogo.jpg"]["outcome"].should == "success"
+
+    # checksum check
+    hash["checksum_check"]["daitss.jpg"]["checksum_match"].should == "success"
+    hash["checksum_check"]["diamondlogo.jpg"]["checksum_match"].should == "success"
+
+    hash["checksum_check"]["daitss.jpg"]["file_exists"].should == "success"
+    hash["checksum_check"]["diamondlogo.jpg"]["file_exists"].should == "success"
   end
 
-  it "validate_pacakge should raise ValidationFailedSyntax if package_name{xml,XML} not present" do
-    mock_paths = "/path/to/package/package_name", "/path/to/package/package_name/content_file.tif"
-    Find.stub!(:find).and_yield(mock_paths[0]).and_yield(mock_paths[1])
+  it "should fail virus check when virus checker returns an exit status equal to virus_exit_status_infected" do
+    Configuration.instance.stub!(:values).and_return virus_found_hash
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedSyntax)
+    hash = @validator.validate_package ALL_OK_PACKAGE
+
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+    hash["syntax"]["descriptor_found"].should == "success"
+    hash["syntax"]["descriptor_is_file"].should == "success"
+    hash["syntax"]["content_file_found"].should == "success"
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"]["FDA0000001.xml"]["outcome"].should == "failure"
+    hash["virus_check"]["FDA0000001.xml"]["STDOUT"].should == "foo\n"
+    hash["virus_check"]["FDA0000001.xml"]["STDERR"].should == "bar\n"
+    hash["virus_check"]["FDA0000001.xml"]["virus_checker_executable"].should == "echo foo; echo bar 1>&2; /usr/bin/false"
+
+    hash["virus_check"]["daitss.jpg"]["outcome"].should == "failure"
+    hash["virus_check"]["daitss.jpg"]["STDOUT"].should == "foo\n"
+    hash["virus_check"]["daitss.jpg"]["STDERR"].should == "bar\n"
+    hash["virus_check"]["daitss.jpg"]["virus_checker_executable"].should == "echo foo; echo bar 1>&2; /usr/bin/false"
+
+    hash["virus_check"]["diamondlogo.jpg"]["outcome"].should == "failure"
+    hash["virus_check"]["diamondlogo.jpg"]["STDOUT"].should == "foo\n"
+    hash["virus_check"]["diamondlogo.jpg"]["STDERR"].should == "bar\n"
+    hash["virus_check"]["diamondlogo.jpg"]["virus_checker_executable"].should == "echo foo; echo bar 1>&2; /usr/bin/false"
+
+    # checksum check
+    hash["checksum_check"]["daitss.jpg"]["checksum_match"].should == "success"
+    hash["checksum_check"]["diamondlogo.jpg"]["checksum_match"].should == "success"
+
+    hash["checksum_check"]["daitss.jpg"]["file_exists"].should == "success"
+    hash["checksum_check"]["diamondlogo.jpg"]["file_exists"].should == "success"
   end
 
-  it "validate_pacakge should raise ValidationFailedSyntax if SIP descriptor is not a file" do
-    File.stub!(:file?).and_return false
+  it "should fail package syntax check if path provided does not exist on the filesystem" do
+    hash = @validator.validate_package DOES_NOT_EXIST_ON_FILESYSTEM
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedSyntax)
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+    
+    hash["syntax"]["descriptor_found"].should == nil
+    hash["syntax"]["descriptor_is_file"].should == nil
+    hash["syntax"]["content_file_found"].should == nil
+    hash["syntax"]["package_is_directory"].should == "failure"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"].should == nil
+
+    # checksum check
+    hash["checksum_check"].should == nil
   end
 
-  it "validate_pacakge should raise ValidationFailedSyntax if there are no content files" do
-    File.stub!(:file?).and_return true, false
+  it "should fail package syntax check if path provided does not point to a directory on the filesystem" do
+    hash = @validator.validate_package NOT_A_DIRECTORY_ON_FILESYSTEM
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedSyntax)
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+
+    hash["syntax"]["descriptor_found"].should == nil
+    hash["syntax"]["descriptor_is_file"].should == nil
+    hash["syntax"]["content_file_found"].should == nil
+    hash["syntax"]["package_is_directory"].should == "failure"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"].should == nil
+
+    # checksum check
+    hash["checksum_check"].should == nil
   end
 
-  it "validate_package should raise ValidationFailedAccountProject if account/project are invalid" do
-    pending "need to implement account project check"
+  it "should fail package syntax check if path provided does not contain a descriptor" do
+    hash = @validator.validate_package DOES_NOT_CONTAIN_DESCRIPTOR
+
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+
+    hash["syntax"]["descriptor_found"].should == "failure"
+    hash["syntax"]["descriptor_is_file"].should == nil
+    hash["syntax"]["content_file_found"].should == nil
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"].should == nil
+
+    # checksum check
+    hash["checksum_check"].should == nil
   end
 
-  it "validate_package should succesfully check account/project if account/project are valid" do
-    pending "need to implement account project check"
+  it "should fail package syntax check if path provided contains a descriptor, but that descriptor is not a file" do
+    hash = @validator.validate_package DESCRIPTOR_IS_NOT_FILE
+
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+
+    hash["syntax"]["descriptor_found"].should == "success"
+    hash["syntax"]["descriptor_is_file"].should == "failure"
+    hash["syntax"]["content_file_found"].should == nil
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"].should == nil
+
+    # checksum check
+    hash["checksum_check"].should == nil
   end
 
-  it "validate_package should raise ValidationFailedDescriptor if root node is not in METS namespace" do
-    @mock_namespace_node.stub!(:prefix).and_return "FOO" 
+  it "should fail package syntax check if there are no content files in package" do
+    hash = @validator.validate_package NO_CONTENT_FILES
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedDescriptor)
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+
+    hash["syntax"]["descriptor_found"].should == "success"
+    hash["syntax"]["descriptor_is_file"].should == "success"
+    hash["syntax"]["content_file_found"].should == "failure"
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"].should == nil
+
+    # checksum check
+    hash["checksum_check"].should == nil
   end
 
-  it "validate_package should raise ValidationFailedDescriptor if descriptor does not validate against METS schema" do
-    @mock_doc.stub!(:validate_schema).and_return false
+  it "should fail checksum check if there is a file that is referenced, but missing" do
+    hash = @validator.validate_package FILE_REFERENCED_BUT_MISSING
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedDescriptor)
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+
+    hash["syntax"]["descriptor_found"].should == "success"
+    hash["syntax"]["descriptor_is_file"].should == "success"
+    hash["syntax"]["content_file_found"].should == "success"
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"]["FDA0000004.xml"]["outcome"].should == "success"
+    hash["virus_check"]["diamondlogo.jpg"]["outcome"].should == "success"
+
+    # checksum check
+    hash["checksum_check"]["daitss.jpg"]["checksum_match"].should == nil
+    hash["checksum_check"]["diamondlogo.jpg"]["checksum_match"].should == "success"
+
+    hash["checksum_check"]["daitss.jpg"]["file_exists"].should == "failure"
+    hash["checksum_check"]["diamondlogo.jpg"]["file_exists"].should == "success"
   end
 
-  it "validate_package should raise ValidationFailedDescriptor if agreement info is missing from descriptor" do
-    @mock_doc.stub!(:find_first).and_return false
+ it "should fail checksum check if described checksum does not match computed checksum" do
+    hash = @validator.validate_package CHECKSUM_MISMATCH
+    pp hash
 
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedDescriptor)
+    hash["outcome"].should == "failure"
+
+    # package syntax checks
+
+    hash["syntax"]["descriptor_found"].should == "success"
+    hash["syntax"]["descriptor_is_file"].should == "success"
+    hash["syntax"]["content_file_found"].should == "success"
+    hash["syntax"]["package_is_directory"].should == "success"
+
+    # TODO: account/project verification not yet implemented"
+    # TODO: descriptor validation not yet implemented 
+    # TODO: undescribed file check not yet implemented"
+
+    # virus check
+    hash["virus_check"]["FDA0000005.xml"]["outcome"].should == "success"
+    hash["virus_check"]["diamondlogo.jpg"]["outcome"].should == "success"
+
+    # checksum check
+    hash["checksum_check"]["daitss.jpg"]["checksum_match"].should == "success"
+    hash["checksum_check"]["diamondlogo.jpg"]["checksum_match"].should == "failure"
+    hash["checksum_check"]["diamondlogo.jpg"]["described"].should == "8C975DE69A9419B8A02DC985839EA852"
+    hash["checksum_check"]["diamondlogo.jpg"]["computed"].should == "8C975DE69A9419B8A02DC985839EA851"
+
+    hash["checksum_check"]["daitss.jpg"]["file_exists"].should == "success"
+    hash["checksum_check"]["diamondlogo.jpg"]["file_exists"].should == "success"
   end
 
-  it "validate_package should raise ValidationFailedDescriptor if descriptor is not well-formed" do
-    LibXML::XML::Document.stub!(:file).and_raise LibXML::XML::Parser::ParseError
-
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedDescriptor)
-  end
-  
-  it "validate_package should raise ValidationFailedVirusCheck if virus found" do
-    stub_return_status = {"exit_status" => 1, "STDOUT" => "", "STDERR" => ""}
-    Executor.stub!(:execute_return_summary).and_return stub_return_status
-
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedVirusCheck)
-  end
-
-  it "validate_package should raise ValidationFailedVirusCheck if virus checker exit status is indeterminate" do
-    stub_return_status = {"exit_status" => 3, "STDOUT" => "", "STDERR" => ""}
-    Executor.stub!(:execute_return_summary).and_return stub_return_status
-
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedVirusCheck)
-  end
-
-  it "validate_package should raise ValidationFailedChecksum if a file is described but not present in package" do
-    File.stub!(:exists?).and_return false
-
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedChecksum) 
-  end
-
-  it "validate_package should raise ValidationFailedChecksum if there is a checksum mismatch" do
-    Digest::MD5.stub!(:hexdigest).and_return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
-
-    lambda { @validator.validate_package "/path_to_package/UF000001" }.should raise_error(ValidationFailedChecksum) 
-  end
-
-  it "validate_package should return true, and not raise exception any errors is package is valid" do
-    lambda { @valid = @validator.validate_package "/path_to_package/UF000001" }.should_not raise_error
-    @valid.should == true
-  end
 end
