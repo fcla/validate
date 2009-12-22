dd33d7f4029133867e277e20eda8eb17e2a3fc43
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-22 10:23:26 -0500

    going commando

--- bin/driver.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'pp'
-
-$:.unshift File.join(File.dirname(__FILE__), 'lib')
-require 'package_validator'
-
-validator = PackageValidator.new
-pp validator.validate_package(ARGV[0])
--- lib/namespace.rb
+++
@@ -1,5 +0,0 @@
-NS_MAP = {
-  'premis' => 'info:lc/xmlns/premis-v2',
-  'METS' => 'http://www.loc.gov/METS/',
-  'xsi' => 'http://www.w3.org/2001/XMLSchema-instance'
-}
